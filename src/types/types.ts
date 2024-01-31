d6f26939c1ae4cb22cfae54f7a22fe4ffec3785e
Allan <allan_creative@hotmail.com>
2024-01-31 15:36:15 -0300

    feat: add new types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,35 +1,61 @@
-export interface Location {
+export type Location = {
   id: string;
   location: string;
   price: number;
   name: string;
   type: string;
-  reservations?: Reservation[];
+  reservations: Reservation[];
   image: string;
   details: string;
-}
+};
 
-export interface Reservation {
+export type Reservation = {
   id: string;
-  startDate: Date;
-  endDate: Date;
-}
+  startDate: Date | undefined;
+  endDate: Date | undefined;
+};
 
-export interface Booking {
+export type Booking = {
   id: string;
   location: string;
-  startDate: Date;
-  endDate: Date;
+  startDate?: Date;
+  dailyPrice: number;
+  name: string;
+  endDate?: Date;
   price: number;
   image: string;
   numberOfDays: number;
   locationId: string;
-}
+};
 
-export interface GlobalState {
+export type BookModal = {
+  open: boolean;
+  mode: "create" | "edit" | "reset";
+};
+
+export type DateRange = {
+  startDate?: Date;
+  endDate?: Date;
+};
+
+export type Toaster = {
+  open: boolean;
+  message: string;
+};
+
+export type GlobalState = {
   locations: Location[];
   bookings: Booking[];
+  bookModal: BookModal;
+  activeBooking?: Booking;
+  activeLocation?: Location;
   editBooking: (updatedBooking: Booking) => void;
   deleteBooking: (id: string) => void;
   createBooking: (booking: Booking) => void;
-}
+  handleModal: (modal: BookModal) => void;
+  setActiveBooking: (booking: Booking | undefined) => void;
+  setActiveLocation: (location: Location | undefined) => void;
+  createReservation: (id: string, reservation: Reservation) => void;
+  editReservation: (id: string, updatedReservation: Reservation) => void;
+  deleteReservation: (id: string, reservationId: string) => void;
+};
